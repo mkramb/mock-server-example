03316124d2bceeab1318b102be0c88a68ae41270
Mitja Kramberger <mitja.kramberger@primarybid.com>
2024-02-20 06:29:05 +0000

    Initial api test

--- tests/api-tests/src/facts.spec.ts
+++ tests/api-tests/src/facts.spec.ts
@@ -1,5 +1,18 @@
+const API_URL = process.env.API_URL ?? 'http://localhost:3001';
+
+const getFacts = async (): Promise<string[]> => {
+  const response = await fetch(`${API_URL}/facts`, { cache: 'no-store' });
+  return (await response.json()).data ?? [];
+};
+
 describe('Facts API', () => {
-  test('should pass', () => {
-    expect(true).toEqual(true);
+  let facts: string[] = [];
+
+  beforeEach(async () => {
+    facts = await getFacts();
+  });
+
+  test('should pass', async () => {
+    expect(facts.length).toEqual(3);
   });
 });
